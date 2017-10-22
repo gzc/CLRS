7ea7d012099fff5e9e1082d3cae3ac7c9831fe6b
Zhenchao Gan <303542123@qq.com>
2017-10-21 22:14:45 -0700

    Fix some bug

--- C09-Medians-and-Order-Statistics/exercise_code/randomized-select-iterative.cpp
+++ C09-Medians-and-Order-Statistics/exercise_code/randomized-select-iterative.cpp
@@ -1,22 +1,20 @@
 /*************************************************************************
-	> File Name: randomized-select-iterative.cpp
-	> Author: Louis1992
-	> Mail: zhenchaogan@gmail.com
-	> Blog: http://gzc.github.io
-	> Created Time: Sun May 24 11:46:39 2015
+ > File Name: randomized-select-iterative.cpp
+ > Author: Louis1992
+ > Mail: zhenchaogan@gmail.com
+ > Blog: http://gzc.github.io
+ > Created Time: Sun May 24 11:46:39 2015
  ************************************************************************/
 #include<iostream>
+#include <cassert>
 using namespace std;
 
 class Solution {
     
-    int partition(int arr[], int l, int r)
-    {
+    int partition(int arr[], int l, int r) {
         int x = arr[r], i = l;
-        for(int j = l; j <= r - 1; j++)
-        {
-            if (arr[j] <= x)
-            {
+        for(int j = l; j <= r - 1; j++) {
+            if (arr[j] <= x) {
                 swap(arr[i], arr[j]);
                 i++;
             }
@@ -25,8 +23,7 @@ class Solution {
         return i;
     }
     
-    int randomPartition(int arr[], int l, int r)
-    {
+    int randomPartition(int arr[], int l, int r) {
         int n = r-l+1;
         int pivot = rand() % n;
         swap(arr[l + pivot], arr[r]);
@@ -35,23 +32,17 @@ class Solution {
     
 public:
     
-    int kthSmallest(int arr[], int l, int r, int k)
-    {
-        cout << l << " " << r << " " << k << endl;
-        while (k > 0 && k <= r - l + 1)
-        {
+    int kthSmallest(int arr[], int l, int r, int k) {
+        int originalK = k;
+        while (k > 0 && k <= r - l + 1) {
             int pos = randomPartition(arr, l, r);
-            
-            if (pos-l == k-1)
-            return arr[pos];
-            else if(pos-l > k-1)
-            {
+            if (pos-l == k-1) {
+                return arr[pos];
+            } else if(pos-l > k-1) {
                 r = pos-1;
-            }
-            else
-            {
+            } else {
                 l = pos+1;
-                k = k-pos+l-1;
+                k = originalK-l;
             }
         }
         return INT_MAX;
@@ -59,9 +50,14 @@ public:
     
 };
 
-int main()
-{
-    int arr[5] = {2,4,1,-2,8};
+int main() {
+    int arr[7] = {10, 100, 2, 4, 1, -2, 8};
     Solution s;
-    cout << s.kthSmallest(arr, 0, 4, 2);
+    assert(s.kthSmallest(arr, 0, 6, 1) == -2);
+    assert(s.kthSmallest(arr, 0, 6, 2) == 1);
+    assert(s.kthSmallest(arr, 0, 6, 3) == 2);
+    assert(s.kthSmallest(arr, 0, 6, 4) == 4);
+    assert(s.kthSmallest(arr, 0, 6, 5) == 8);
+    assert(s.kthSmallest(arr, 0, 6, 6) == 10);
+    assert(s.kthSmallest(arr, 0, 6, 7) == 100);
 }
